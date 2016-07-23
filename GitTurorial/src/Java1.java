b66ced7547a868d707b24c1de6b36e482c23b9a8
SONY <SONY@192.168.1.6>
2016-07-23 16:36:33 +0530

    revision 2

--- GitTurorial/src/Java1.java
+++ GitTurorial/src/Java1.java
@@ -1,7 +1,10 @@
 
 public class Java1 {
 	private void psvm() {
-		// TODO Auto-generated method stub
-
+		// TODO Auto-generated method stub		
+	}
+	
+	private void psvm1() {
+		// TODO Auto-generated method stub		
 	}
 }
